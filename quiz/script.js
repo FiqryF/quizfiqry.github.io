eaa5a68de03c260c35a47dd979beda3c2b7e19e1
FiqryF <fiqryyw@gmail.com>
2024-11-21 11:05:33 +0700

    KITA SEHAT

---
+++ quiz/script.js
@@ -0,0 +1,6 @@
+// Placeholder JavaScript for interactivity
+document.querySelectorAll(".icon").forEach(icon => {
+    icon.addEventListener("click", () => {
+        alert("Fitur ini sedang dikembangkan!");
+    });
+});
